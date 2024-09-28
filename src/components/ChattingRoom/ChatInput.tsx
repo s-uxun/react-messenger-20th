3000d2131631957306229fd8e5ff0d953b3bdff0
s-uxun <serenasol@ewha.ac.kr>
2024-09-29 00:19:02 +0900

    Fix: onKeyDown -> onKeyUp

--- src/components/ChattingRoom/ChatInput.tsx
+++ src/components/ChattingRoom/ChatInput.tsx
@@ -44,13 +44,7 @@ export function ChatInput({ onNewChat }: { onNewChat: (id: number) => void }) {
   };
 
   // 엔터키로 전송할 수 있도록 처리 (Shift + Enter로 줄바꿈)
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.nativeEvent.isComposing) {
-      // 한글 조합 중일 때는 이벤트 처리를 하지 않도록 함으로써 마지막 글자가 두 번 제출되는 오류 방지
-      e.stopPropagation();
-      return;
-    }
-
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>): void => {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
       handleSend();
@@ -68,7 +62,7 @@ export function ChatInput({ onNewChat }: { onNewChat: (id: number) => void }) {
           ref={textareaRef}
           value={value}
           onChange={handleChange}
-          onKeyDown={handleKeyDown}
+          onKeyUp={handleKeyUp}
         />
         <Icons>
           <StyledIcon as={Emoji} style={{ marginRight: "0.5rem" }} />

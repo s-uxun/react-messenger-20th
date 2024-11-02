49489ef06077068bd9d63e3c125db564cc0a3eba
s-uxun <serenasol@ewha.ac.kr>
2024-11-02 19:47:49 +0900

    Feat: BirthdayUser Publishing

---
+++ src/assets/icons/Right.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgRight = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#888E94"
+      d="M8.3 5.7q-.45-.45-.275-.975.175-.5.725-.675.525-.175.95.25l7.025 7q.325.35.325.7 0 .375-.325.7l-7.025 7q-.425.425-.95.25a1.05 1.05 0 0 1-.7-.7q-.2-.5.25-.95l6.275-6.3z"
+    />
+  </svg>
+);
+export default SvgRight;
--- src/components/FriendList/BirthdayUser.tsx
+++ src/components/FriendList/BirthdayUser.tsx
@@ -1,5 +1,49 @@
+import styled from "styled-components";
+import { GiftCircle } from "../../assets/icons";
+import { Right } from "../../assets/icons";
+
 const BirthdayUser = () => {
-  return <p>birthdayuser</p>;
+  return (
+    <Container>
+      <Title>생일인 친구</Title>
+      <Content>
+        <GiftCircle style={{ width: "2.75rem", height: "2.75rem" }} />
+        <Notice>친구의 생일을 확인해 보세요!</Notice>
+        <Right
+          style={{
+            width: "1.5rem",
+            height: "1.5ren",
+            position: "absolute",
+            right: "0",
+          }}
+        />
+      </Content>
+    </Container>
+  );
 };
 
 export default BirthdayUser;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  padding: 0.75rem 1.25rem;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  position: relative;
+`;
+
+const Title = styled.p`
+  ${({ theme }) => theme.font.Caption_med}
+  color: ${({ theme }) => theme.color.gray50}
+`;
+
+const Notice = styled.p`
+  ${({ theme }) => theme.font.Body_1_med}
+  color: ${({ theme }) => theme.color.gray100}
+`;

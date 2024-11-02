0e24a4fd7911777868f525a52f00ca564f45a661
s-uxun <serenasol@ewha.ac.kr>
2024-11-02 15:17:01 +0900

    Feat: set currentUser logic in FriendList.tsx

--- src/components/FriendList/AllFriends.tsx
+++ src/components/FriendList/AllFriends.tsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import useUserStore, { User } from "../../stores/UserStore";
+import useChatroomStore from "../../stores/ChatroomStore";
 import UserInfo from "./UserInfo";
 import UpdatedProfile from "./UpdatedProfile";
 import BirthdayUser from "./BirthdayUser";
 
-const AllFriends = () => {
-  const users = useUserStore((state) => state.users);
-  const currentUser = users.find((user) => user.id === 1);
+interface AllFriendsProps {
+  currentUser: User | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+const AllFriends: React.FC<AllFriendsProps> = ({
+  currentUser,
+  setCurrentUser,
+}) => {
+  const navigate = useNavigate();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <Container>
--- src/pages/FriendList.tsx
+++ src/pages/FriendList.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import useUserStore, { User } from "../stores/UserStore";
 import styled from "styled-components";
 import { MobileTop } from "../components/MobileTop";
 import { ListTitle } from "../components/ChattingList/ListTitle";
@@ -6,12 +8,33 @@ import AllFriends from "../components/FriendList/AllFriends";
 import MenuBar from "../components/ChattingList/MenuBar";
 
 const FriendList = () => {
+  const users = useUserStore((state) => state.users);
+  const [currentUser, setCurrentUser] = useState<User | null>(
+    users.find((user) => user.id === 1) || null
+  );
+  // 일단 id값이 1인 내 데이터를 기본 currentUser로 설정
+
+  // 로컬에서 currentUser 불러옴
+  useEffect(() => {
+    const storedUser = localStorage.getItem("currentUser");
+    if (storedUser) {
+      setCurrentUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  // currentUser 로컬에 저장
+  useEffect(() => {
+    if (currentUser) {
+      localStorage.setItem("currentUser", JSON.stringify(currentUser));
+    }
+  }, [currentUser]);
+
   return (
     <Wrapper>
       <MobileTop />
       <ListTitle />
       <ContentStyle>
-        <AllFriends />
+        <AllFriends currentUser={currentUser} setCurrentUser={setCurrentUser} />
       </ContentStyle>
       <MenuBar />
       <ChatBottom />
